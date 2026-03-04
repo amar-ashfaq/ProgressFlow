24a5f22bd5614ef38c3900a9f9311f10f40a08e2
Amar AA <amar.ashfaq95@gmail.com>
2026-03-04 17:54:57 +0000

    Implemented the user repository layer

---
+++ Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using ProgressFlow.Entities;
+
+namespace ProgressFlow.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetUserByIdAsync(int id);
+        Task<User?> GetUserByUsernameAsync(string username);
+        Task<User?> GetUserByEmailAsync(string email);
+        Task<User> CreateUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserByIdAsync(int id);
+    }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgressFlow.Data;
+using ProgressFlow.Entities;
+using ProgressFlow.Repositories.Interfaces;
+
+namespace ProgressFlow.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetUserByIdAsync(int id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+            return user;
+        }
+
+        public async Task<User?> GetUserByUsernameAsync(string username)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+            return user;
+        }
+
+        public async Task<User?> GetUserByEmailAsync(string email)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Email == email);
+            return user;
+        }
+
+        public async Task<User> CreateUserAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteUserByIdAsync(int id)
+        {
+            var user = await GetUserByIdAsync(id);
+
+            if (user == null)
+            {
+                return;
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
